1d4e137dd1387a41d6a9f9bba68468a6c7731edf
r2road <studior2road@gmail.com>
2020-05-04 20:32:47 +0900

    add : 008 EnvironmentVariable 04 add n remove with target..................... maybe you want this

---
+++ 008_EnvironmentVariable_04_add_n_remove_with_target.ps1
@@ -0,0 +1,46 @@
+﻿cls
+
+
+
+# Add Environment Variable
+echo "`n"
+echo '>>> [Environment]::SetEnvironmentVariable( ''TEST'',''Test'', [System.EnvironmentVariableTarget]::User ) <<<'
+echo "`n"
+
+# Is Same >> [Environment]::SetEnvironmentVariable( 'TEST','Test', "User" )
+[Environment]::SetEnvironmentVariable( 'TEST','Test', [System.EnvironmentVariableTarget]::User )
+
+echo "`n"
+pause
+
+
+
+echo "`n"
+
+[Environment]::GetEnvironmentVariable( 'TEST', [EnvironmentVariableTarget]::User )
+
+echo "`n"
+pause
+
+
+
+# Remove Environment Variable
+echo "`n"
+echo '>>> [Environment]::SetEnvironmentVariable( ''TEST'',$null, [System.EnvironmentVariableTarget]::User ) <<<'
+echo "`n"
+
+[Environment]::SetEnvironmentVariable( 'TEST',$null, [System.EnvironmentVariableTarget]::User )
+
+echo "`n"
+pause
+
+
+
+echo "`n"
+
+[Environment]::GetEnvironmentVariable( 'TEST', [EnvironmentVariableTarget]::User )
+
+echo "`n"
+pause
+
+cls

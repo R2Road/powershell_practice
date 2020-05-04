ec58956f19c5d5f212e90b43e74ea7c4e5a7aa34
r2road <studior2road@gmail.com>
2020-05-04 12:59:52 +0900

    update : 002 Directory 01

--- 002_Directory_01.ps1
+++ 002_Directory_01.ps1
@@ -1,8 +1,26 @@
-﻿Get-ChildItem
+﻿cls
+
+echo "`n"
+
+Get-ChildItem
+
+echo "`n"
 
 pause
 
+echo "`n"
+
 Get-ChildItem | select Name
+
+echo "`n"
+
+pause
+
+echo "`n"
+
+Get-ChildItem | select FullName
+
+echo "`n"
 
 pause
 

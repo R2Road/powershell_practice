22d53e26438d4b43099df088d02f29a5332c3a73
r2road <studior2road@gmail.com>
2020-05-04 20:46:08 +0900

    add : 008 EnvironmentVariable 03

---
+++ 008_EnvironmentVariable_03.ps1
@@ -0,0 +1,19 @@
+﻿cls
+
+echo "`n>>> Set-Location Env: <<<`n"
+
+Set-Location Env:
+
+echo "`n"
+pause
+
+
+
+echo "`n`n`n>>> Get-Content -Path APPDATA <<<`n"
+
+Get-Content -Path APPDATA
+
+echo "`n"
+pause
+
+cls

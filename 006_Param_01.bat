74bd778990d55a7adf04ffba668496333602da77
r2road <studior2road@gmail.com>
2020-05-04 11:13:45 +0900

    add : 006 Param 01

---
+++ 006_Param_01.bat
@@ -0,0 +1,5 @@
+@echo off
+
+Powershell.exe -noprofile -executionpolicy bypass -file "./006_Param_01.ps1" "006" "Param" "01" 
+
+exit
---
+++ 006_Param_01.ps1
@@ -0,0 +1,19 @@
+﻿param($temp1, $temp2, $temp3)
+
+echo "`n"
+
+$temp1
+
+echo "`n"
+
+$temp2
+
+echo "`n"
+
+$temp3
+
+echo "`n"
+
+pause
+
+cls

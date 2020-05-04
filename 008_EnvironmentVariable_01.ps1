04bc39d5d40fa9adf7bacb0db07c6acc374f8c70
r2road <studior2road@gmail.com>
2020-05-04 14:09:10 +0900

    add : 008 EnvironmentVariable 01, 02

---
+++ 008_EnvironmentVariable_01.ps1
@@ -0,0 +1,19 @@
+﻿cls
+
+echo "`n"
+
+Get-Variable HOME
+
+echo "`n"
+
+pause
+
+echo "`n"
+
+Get-Variable HOME -Valueonly
+
+echo "`n"
+
+pause
+
+cls
---
+++ 008_EnvironmentVariable_02.ps1
@@ -0,0 +1,23 @@
+﻿#https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/get-variable?view=powershell-7
+
+cls
+
+echo "`n"
+
+echo "### Get-Variable H* ###"
+Get-Variable H*
+
+echo "`n"
+
+pause
+
+echo "`n"
+
+echo "### Get-Variable -Scope 0 ###"
+Get-Variable -Scope 0
+
+echo "`n"
+
+pause
+
+cls

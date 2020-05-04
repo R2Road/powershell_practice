c772aef75196bbffbe14a64196ed5adfdde64f18
r2road <studior2road@gmail.com>
2020-05-04 10:44:14 +0900

    ...

--- 004_OutFile_01.ps1
+++ 004_OutFile_01.ps1
@@ -1,4 +1,6 @@
-﻿echo "`n"
+﻿# https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/out-file?view=powershell-7
+
+echo "`n"
 
 Get-ChildItem | Out-File .\002_Directory_02_test.txt
 

5ea0baa07b1c93b292979e88e22f3cb9bf445911
r2road <studior2road@gmail.com>
2020-05-04 12:54:02 +0900

    add : 002 Directory 02

---
+++ 002_Directory_02.ps1
@@ -0,0 +1,11 @@
+﻿cls
+
+echo "`n"
+
+# %{} block is called for every value in the array.
+# The $_ or $PSItem variable will contain the current value.
+Get-ChildItem | %{ $_.Name }
+
+echo "`n"
+
+pause

add1e2463a104b8b128de808c85a48526d4ed7e1
r2road <studior2road@gmail.com>
2020-05-04 10:37:12 +0900

    add : 003 Script Execution Directory 01, 02

---
+++ 003_ScriptExecutionDirectory_01.ps1
@@ -0,0 +1,16 @@
+﻿# From : https://stackoverflow.com/questions/5466329/whats-the-best-way-to-determine-the-location-of-the-current-powershell-script
+
+function Get-ScriptDirectory {
+    if ($psise) {
+        Split-Path $psise.CurrentFile.FullPath
+    }
+    else {
+        $global:PSScriptRoot
+    }
+}
+
+Get-ScriptDirectory | Out-String
+
+pause
+
+cls
---
+++ 003_ScriptExecutionDirectory_02.ps1
@@ -0,0 +1,17 @@
+﻿# $PSCommandPath
+#    Contains the full path and file name of the script that is being run. 
+#    This variable is valid in all scripts.
+
+function Get-ScriptDirectory {
+    Split-Path -Parent $PSCommandPath
+}
+
+echo "`n"
+
+Get-ScriptDirectory
+
+echo "`n"
+
+pause
+
+cls

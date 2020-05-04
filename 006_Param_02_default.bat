bac1f2ed388da7f39eb41add257bae7b1d01154c
r2road <studior2road@gmail.com>
2020-05-04 11:16:32 +0900

    add : 006 Param 02 default

---
+++ 006_Param_02_default.bat
@@ -0,0 +1,5 @@
+@echo off
+
+Powershell.exe -noprofile -executionpolicy bypass -file "./006_Param_02_default.ps1"
+
+exit
---
+++ 006_Param_02_default.ps1
@@ -0,0 +1,19 @@
+﻿param($temp1 = "default 1", $temp2 = "default 2", $temp3 = "default 3")
+
+echo "`n"
+
+$temp1
+
+echo "`n"
+
+$temp2
+
+echo "`n"
+
+$temp3
+
+echo "`n"
+
+pause
+
+cls

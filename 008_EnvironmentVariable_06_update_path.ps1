b62779fe9a692a65a3c89e15f23ee1a2c23bfd16
r2road <studior2road@gmail.com>
2020-05-05 07:35:45 +0900

    add : 008 EnvironmentVariable 06 update path

---
+++ 008_EnvironmentVariable_06_update_path.ps1
@@ -0,0 +1,69 @@
+﻿cls
+
+echo "`n >>> Get Target Path <<<"
+
+$target_exe = "notepad.exe"
+$target_path = Get-Command $target_exe -ErrorAction Ignore | %{ $_.Path }
+$target_path
+
+echo "`n"
+
+if( !$target_path )
+{
+    throw 'exe Path Not Found'
+}
+
+pause
+
+
+
+echo "`n >>> User Path <<<"
+
+$user_path = [Environment]::GetEnvironmentVariable( "path", [System.EnvironmentVariableTarget]::User )
+$user_path
+
+echo "`n`n"
+
+if( $user_path | Select-String -Pattern $target_path -SimpleMatch )
+{
+    throw 'exe Path Already added'
+}
+
+pause
+
+
+
+echo "`n >>> Update Target Path <<<"
+
+$target_path = $target_path -replace $target_exe, ""
+$target_path += ";"
+$target_path
+
+echo "`n`n"
+pause
+
+
+
+echo "`n >>> Result User Path <<<"
+[Environment]::SetEnvironmentVariable(
+    "TEST"
+    , $target_path + $user_path
+    , [System.EnvironmentVariableTarget]::User
+)
+
+[Environment]::GetEnvironmentVariable( "TEST", [System.EnvironmentVariableTarget]::User )
+
+echo "`n`n"
+pause
+
+
+
+echo "`n >>> Remove Environment Variable <<<"
+
+[Environment]::SetEnvironmentVariable( "TEST", $null, [System.EnvironmentVariableTarget]::User )
+[Environment]::GetEnvironmentVariable( "TEST", [System.EnvironmentVariableTarget]::User )
+
+echo "`n`n"
+pause
+
+cls

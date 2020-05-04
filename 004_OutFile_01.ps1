07c25552f749d00fd3e362856a08a81d90414c23
r2road <studior2road@gmail.com>
2020-05-04 14:38:50 +0900

    cleanup

--- 004_OutFile_01.ps1
+++ 004_OutFile_01.ps1
@@ -1,9 +1,13 @@
 ﻿# https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/out-file?view=powershell-7
 
-echo "`n"
-
+echo "`n>>> Get-ChildItem | Out-File .\002_Directory_02_test.txt <<<`n"
 Get-ChildItem | Out-File .\002_Directory_02_test.txt
 
+echo "`n"
+
+pause
+
+echo "`n>>> Get-Content -Path .\002_Directory_02_test.txt <<<`n"
 Get-Content -Path .\002_Directory_02_test.txt
 
 echo "`n"

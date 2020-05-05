9de049580a04b7e0b5cccf45d0223d266289d44a
r2road <studior2road@gmail.com>
2020-05-06 06:52:42 +0900

    add : 103 AssemblyName 01 messagebox

---
+++ 103_AssemblyName_01_messagebox.ps1
@@ -0,0 +1,11 @@
+﻿cls
+
+echo "`n>>> C# - Add-Type -AssemblyName 1 <<<`n"
+
+Add-Type -AssemblyName PresentationFramework
+[System.Windows.MessageBox]::Show('Do you like powershell?','Question','YesNo','Question')
+
+echo "`n"
+pause
+
+cls

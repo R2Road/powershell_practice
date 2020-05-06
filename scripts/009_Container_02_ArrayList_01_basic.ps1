5f234159dc9bd3975d534473c14745cd8e987859
r2road <studior2road@gmail.com>
2020-05-07 01:48:20 +0900

    add : 009 Container 02 ArrayList 01 basic

---
+++ scripts/009_Container_02_ArrayList_01_basic.ps1
@@ -0,0 +1,52 @@
+﻿cls
+
+
+
+echo '>>> $temp_arraylist_1 =  New-Object -TypeName System.Collections.ArrayList <<<'
+
+$temp_arraylist_1 = New-Object -TypeName System.Collections.ArrayList
+
+echo "`n"
+
+
+
+echo '>>> $temp_arraylist_1.GetType() <<<'
+
+$temp_arraylist_1.GetType()
+
+echo "`n`n`n`n"
+
+
+
+
+
+
+# Cast : Array > ArrayList
+echo '>>> $temp_arraylist_2 =[System.Collections.ArrayList]@(1..4) <<<'
+
+$temp_arraylist_2 = [System.Collections.ArrayList]@(1..4)
+
+echo "`n"
+
+
+
+echo '>>> echo $temp_arraylist_2 <<<'
+
+echo $temp_arraylist_2
+
+echo "`n"
+
+
+
+echo '>>> $temp_arraylist_2.GetType() <<<'
+
+$temp_arraylist_2.GetType()
+
+echo "`n"
+
+
+
+echo "`n"
+pause
+
+cls

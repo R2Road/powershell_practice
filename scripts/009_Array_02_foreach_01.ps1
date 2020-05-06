4778fff143afbe5d01dc5d9f91a1472b41f9cb4d
r2road <studior2road@gmail.com>
2020-05-06 15:03:37 +0900

    add : 009 array 02 foreach 01

---
+++ scripts/009_Array_02_foreach_01.ps1
@@ -0,0 +1,31 @@
+﻿cls
+
+echo '>>> $temp_array = @( 0..10 ) <<<'
+
+$temp_array = @( 0..10 )
+
+echo "`n"
+
+
+
+echo '>>> $temp_array.ForEach( { $sum += $_ } ) <<<'
+
+$sum = 0
+$temp_array.ForEach( { $sum += $_ } )
+
+echo "`n"
+
+
+
+echo '>>> $sum <<<'
+
+$sum
+
+echo "`n"
+
+
+
+echo "`n"
+pause
+
+cls

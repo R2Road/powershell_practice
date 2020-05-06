0216d04478b5d98f884eebb4b74821a07fe097e2
r2road <studior2road@gmail.com>
2020-05-06 17:59:28 +0900

    add : 000 Version 01

---
+++ scripts/000_Version_01.ps1
@@ -0,0 +1,57 @@
+﻿# ref : https://adamtheautomator.com/check-powershell-version/
+
+cls
+
+echo '>>> $PSVersionTable.PSVersion <<<' #Suggest
+
+$PSVersionTable.PSVersion
+
+echo "`n`n"
+
+
+
+echo '>>> (Get-Host).Version <<<'
+
+(Get-Host).Version
+
+echo "`n`n"
+
+
+
+echo '>>> $host.Version <<<'
+
+$host.Version
+
+echo "`n`n"
+
+
+
+echo '>>> Registry : Get-ItemProperty 1 <<<'
+
+(Get-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\PowerShell\3\PowerShellEngine -Name 'PowerShellVersion').PowerShellVersion
+
+echo "`n`n"
+
+
+
+echo '>>> Registry : Get-ItemProperty 2 <<<'
+
+[version](Get-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\PowerShell\3\PowerShellEngine -Name 'PowerShellVersion').PowerShellVersion
+
+echo "`n`n"
+
+
+
+
+echo '>>> Registry : reg query <<<'
+
+reg query HKLM\SOFTWARE\Microsoft\PowerShell\3\PowerShellEngine /v PowerShellVersion
+
+echo "`n`n"
+
+
+
+echo "`n"
+pause
+
+cls

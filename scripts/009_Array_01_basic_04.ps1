9eccbb1cf218226cfcb7c0c764f5aec8353e1fa4
r2road <studior2road@gmail.com>
2020-05-06 14:54:29 +0900

    add : 009 array 01 basic 04

---
+++ scripts/009_Array_01_basic_04.ps1
@@ -0,0 +1,46 @@
+﻿cls
+
+echo '>>> $temp_array = @() <<<'
+
+$temp_array = @()
+
+echo "`n"
+
+
+
+echo '>>> $temp_array.GetType() <<<'
+
+$temp_array.GetType()
+
+echo "`n`n`n`n`n"
+
+
+
+echo '>>> [string[]]$temp_array2 = @( "a", "b", "c" ) <<<'
+
+[string[]]$temp_array2 = @( "a", "b", "c" )
+
+echo "`n"
+
+
+
+echo '>>> echo $temp_array2 <<<'
+
+echo $temp_array2
+
+echo "`n"
+
+
+
+echo '>>> $temp_array2.GetType() <<<'
+
+$temp_array2.GetType()
+
+echo "`n"
+
+
+
+echo "`n"
+pause
+
+cls

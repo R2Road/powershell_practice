88cec31beeb75285b90b8e1052d758ddca15d9cc
r2road <studior2road@gmail.com>
2020-05-07 14:20:57 +0900

    add : 010 Path 04 join path

---
+++ scripts/010_Path_04_join_path.ps1
@@ -0,0 +1,67 @@
+﻿# ref : https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/join-path?view=powershell-5.1
+
+cls
+
+
+
+echo "`n"
+echo '>>> Join-Path  -Path "head"  -ChildPath "child" <<<'
+echo "`n"
+
+Join-Path -Path "head" -ChildPath "child"
+
+echo "`n"
+
+
+
+echo "`n"
+echo '>>> Join-Path  -Path "head\"  -ChildPath "\child" <<<'
+echo "`n"
+
+Join-Path -Path "head\"  -ChildPath "\child"
+
+echo "`n"
+
+
+
+echo "`n"
+echo '>>> Join-Path -Path C:, D:, E: -ChildPath New <<<'
+echo "`n"
+
+Join-Path -Path C:, D:, E: -ChildPath New
+
+echo "`n"
+
+
+
+echo "`n"
+echo '>>> Join-Path "C:\win*" "System*" -Resolve <<<'
+echo "`n"
+
+# Search And Build
+Join-Path "C:\win*" "System*" -Resolve
+
+echo "`n"
+
+
+
+echo "`n"
+echo '>>> Get-PSDrive <<<'
+
+Get-PSDrive
+
+echo "`n"
+echo '>>> Get-PSDrive -PSProvider filesystem | ForEach-Object {$_.root} | Join-Path -ChildPath "Subdir" <<<'
+echo "`n"
+
+Get-PSDrive -PSProvider filesystem | ForEach-Object {$_.root} | Join-Path -ChildPath "Subdir"
+
+echo "`n"
+
+
+
+
+echo "`n"
+pause
+
+cls

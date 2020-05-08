47a2fb4e98b4582beb04f2a78c750917e85ccfde
r2road <studior2road@gmail.com>
2020-05-09 08:31:22 +0900

    add : 011 String 03 replace

---
+++ scripts/011_String_03_replace.ps1
@@ -0,0 +1,63 @@
+﻿#ref : https://devblogs.microsoft.com/scripting/use-powershell-to-replace-text-in-strings/
+
+cls
+
+
+
+echo "`n"
+echo '>>> Introduce Variable <<<'
+echo "`n"
+
+
+
+$orig_string = "hello powershell!"
+$temp_string = ""
+
+echo '$orig_string = "hello powershell"'
+echo '$temp_string = ""'
+echo "`n"
+
+
+
+echo "`n"
+echo '>>> $temp_string = $orig_string <<<'
+echo '>>> $temp_string.Replace( "powershell", "world" ) <<<'
+echo "`n"
+
+$temp_string = $orig_string
+$temp_string.Replace( "powershell", "world" )
+
+echo "`n"
+
+
+
+echo "`n"
+echo '>>> $temp_string = $orig_string <<<'
+echo '>>> $temp_string -replace "powershell", "world" <<<'
+echo "`n"
+
+$temp_string = $orig_string
+$temp_string -replace "powershell", "world"
+
+echo "`n"
+
+
+
+echo "`n"
+echo '>>> $temp_string = $orig_string <<<'
+echo '>>> $temp_string -replace "powershell$", "world" <<<'
+echo "`n"
+
+$temp_string = $orig_string
+
+# $ is match has to occur at the end of the string
+$temp_string -replace "powershell$", "world"
+
+echo "`n"
+
+
+
+echo "`n"
+pause
+
+cls

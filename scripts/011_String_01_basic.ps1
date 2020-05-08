7bef93e6ee37e5662960f6f1d92226668fc32e1b
r2road <studior2road@gmail.com>
2020-05-09 07:49:48 +0900

    add : 011 String 01 basic

---
+++ scripts/011_String_01_basic.ps1
@@ -0,0 +1,42 @@
+﻿cls
+
+
+
+echo "`n"
+echo '>>> Definition <<<'
+echo "`n"
+
+
+
+echo '>>> $temp_string_1 = "aa bb cc dd ee ff" <<<'
+
+$temp_string_1 = "aa bb cc dd ee ff"
+$temp_string_1
+
+echo "`n"
+
+
+
+echo '>>> [string]$temp_string_2 = "11 22 33 44 55 66" <<<'
+
+[string]$temp_string_2 = "11 22 33 44 55 66"
+$temp_string_2
+
+echo "`n"
+
+
+
+echo '>>> [string]$temp_string_3 = 1 <<<'
+
+[string]$temp_string_3 = 123.456
+$temp_string_3.GetType()
+$temp_string_3
+
+echo "`n"
+
+
+
+echo "`n"
+pause
+
+cls

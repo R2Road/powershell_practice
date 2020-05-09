8eabcec277a0619765bc6405bd6827a5cbfe972f
r2road <studior2road@gmail.com>
2020-05-09 17:22:43 +0900

    add : 002 Directory 02 ls 01, 02

---
+++ scripts/002_Directory_02_ls_01.ps1
@@ -0,0 +1,24 @@
+﻿cls
+
+echo "`n"
+
+ls
+
+echo "`n"
+pause
+
+echo "`n"
+
+ls | select name
+
+echo "`n"
+pause
+
+echo "`n"
+
+ls | select fullname
+
+echo "`n"
+pause
+
+cls
---
+++ scripts/002_Directory_02_ls_02.ps1
@@ -0,0 +1,23 @@
+﻿cls
+
+
+
+echo "`n"
+
+ls | %{ $_.Name }
+
+echo "`n"
+pause
+
+
+
+echo "`n"
+
+ls | %{ $_.FullName }
+
+echo "`n"
+pause
+
+
+
+cls

30d697b07cf6a2459d09dfdd6c79f4b8f6e42581
r2road <studior2road@gmail.com>
2020-05-16 10:04:32 +0900

    add : 012 statement 02 loop 01 while

---
+++ scripts/012_Statement_02_Loop_01_while.ps1
@@ -0,0 +1,34 @@
+﻿# About Comparison Operators : https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_comparison_operators?view=powershell-7
+
+<#
+-eq	equals
+-ne	not equals
+-gt	greater than
+-ge	greater than or equal
+-lt	less than
+-le	less than or equal
+#>
+
+cls
+
+
+
+echo "`n"
+echo '>>> while 0 to 10 -ne <<<'
+echo "`n"
+
+$i = 0
+while( $i -ne 10 )
+{
+    echo $i
+
+    ++$i
+}
+echo "`n"
+
+
+
+echo "`n"
+pause
+
+cls

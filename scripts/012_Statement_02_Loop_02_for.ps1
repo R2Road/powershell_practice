13e137e1a19effe4da106502de4c97a856215dbc
r2road <studior2road@gmail.com>
2020-05-16 10:01:18 +0900

    add : 012 statement 02 loop 02 for

---
+++ scripts/012_Statement_02_Loop_02_for.ps1
@@ -0,0 +1,31 @@
+﻿# About Comparison Operators : https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_comparison_operators?view=powershell-7
+
+<#
+-eq	equals
+-ne	not equals
+-gt	greater than
+-ge	greater than or equal
+-lt	less than
+-le	less than or equal
+#>
+
+cls
+
+
+
+echo "`n"
+echo '>>> for 0 to 10 -lt <<<'
+echo "`n"
+
+for( $i = 0; $i -lt 10;  ++$i )
+{
+    echo $i
+}
+echo "`n"
+
+
+
+echo "`n"
+pause
+
+cls

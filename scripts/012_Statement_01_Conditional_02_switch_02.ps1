a4f261d6543e9f0de4d38fda10c02089a3c0317b
r2road <studior2road@gmail.com>
2020-05-18 03:31:56 +0900

    add : 012 statement -1 conditional 02 switch 02

---
+++ scripts/012_Statement_01_Conditional_02_switch_02.ps1
@@ -0,0 +1,30 @@
+﻿#ref : https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_switch?view=powershell-7
+
+cls
+
+
+
+$file_path = Join-Path ( Split-Path -Parent $PSCommandPath ) "012_Statement_01_Conditional_02_switch_02.txt"
+
+echo "`n"
+echo '>>> switch with file<<<'
+echo ">>> file_path = $file_path <<<"
+echo "`n"
+
+
+
+switch -file $file_path
+{
+    1 { echo "case 1" }
+    2 { echo "case 2" }
+    3 { echo "case 3" }
+    default { "case default $_" }
+}
+echo "`n"
+
+
+
+echo "`n"
+pause
+
+cls

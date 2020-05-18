9582be8ff0f88a2ea7fcb07e78f04fd025b97def
r2road <studior2road@gmail.com>
2020-05-19 05:51:58 +0900

    add : 013 Assembly 02 Forms 01 form 01

---
+++ scripts/103_Assembly_02_Forms_02_form_01.ps1
@@ -0,0 +1,18 @@
+﻿cls
+
+echo "`n"
+echo ">>> Form 01 <<<"
+echo "`n"
+
+Add-Type -AssemblyName System.Windows.Forms
+
+$form = New-Object System.Windows.Forms.Form
+$form.Text = 'Form 01'
+$form.Size = New-Object System.Drawing.Size( 330,330 )
+$form.WindowState = "Normal" #Maximized, Minimized
+$form.StartPosition = 'CenterScreen'
+$form.MinimizeBox = $False
+$form.MaximizeBox = $False
+$form.KeyPreview = $True
+
+$form.ShowDialog()

0428655507feb51233003dcc6efc892bc6af707b
r2road <studior2road@gmail.com>
2020-05-19 06:00:15 +0900

    add : 013 Assembly 02 Forms 01 form 02 show image

---
+++ scripts/103_Assembly_02_Forms_02_form_02_show_image.ps1
@@ -0,0 +1,33 @@
+﻿cls
+
+echo "`n"
+echo ">>> Forms : Form 02 : Show Image <<<"
+echo "`n"
+
+Add-Type -AssemblyName System.Windows.Forms
+
+$form = New-Object System.Windows.Forms.Form
+$form.Text = 'Form 02 : Show Image'
+$form.Size = New-Object System.Drawing.Size( 330,330 )
+$form.WindowState = "Normal" #Maximized, Minimized
+$form.StartPosition = 'CenterScreen'
+$form.MinimizeBox = $False
+$form.MaximizeBox = $False
+$form.KeyPreview = $True
+
+
+
+$file_path = Join-Path ( Split-Path -Parent $PSCommandPath ) "103_Assembly_02_Forms_02_form_rsc_01.png"
+$image = [System.Drawing.Image]::FromFile( $file_path )
+
+
+
+$picture_box = new-object System.Windows.Forms.PictureBox
+$picture_box.Width = $image.Size.Width
+$picture_box.Height = $image.Size.Height
+$picture_box.Image = $image
+$form.controls.add( $picture_box )
+
+
+
+$form.ShowDialog()

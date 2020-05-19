ae6d793ad1462838ce33938912870fd2e67c3e35
r2road <studior2road@gmail.com>
2020-05-19 11:29:08 +0900

    013 Random 01, 02

---
+++ scripts/013_Random_01_Get_Random.ps1
@@ -0,0 +1,19 @@
+﻿cls
+
+echo "`n"
+echo ">>> Random 01 : Get-Random <<<"
+echo "`n"
+
+for ($i = 0; $i -lt 10; ++$i)
+{
+    echo ( Get-Random )
+}
+
+echo "`n"
+
+for ($i = 0; $i -lt 10; ++$i)
+{
+    echo ( Get-Random -Minimum 0 -Maximum 100 )
+}
+
+echo "`n"
---
+++ scripts/013_Random_02_System_Random.ps1
@@ -0,0 +1,21 @@
+﻿cls
+
+echo "`n"
+echo ">>> Random 02 : System.Random <<<"
+echo "`n"
+
+$random = New-Object System.Random
+
+for ($i = 0; $i -lt 10; ++$i)
+{
+    echo $random.Next()
+}
+
+echo "`n"
+
+for ($i = 0; $i -lt 10; ++$i)
+{
+    echo $random.Next( 0, 100 )
+}
+
+echo "`n"

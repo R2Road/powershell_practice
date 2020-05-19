ea99f8540a20da5c9c1193bdfe495da1ae414005
r2road <studior2road@gmail.com>
2020-05-19 11:37:07 +0900

    update : 013 Random 01 Get Random 02

--- scripts/013_Random_01_Get_Random_02.ps1
+++ scripts/013_Random_01_Get_Random_02.ps1
@@ -6,7 +6,16 @@ echo "`n"
 
 for ($i = 0; $i -lt 10; ++$i)
 {
-    echo ( 1..100 | Get-Random )
+    echo ( 1..100 | Get-Random ) # not good
+}
+
+echo "`n"
+
+
+$array = @( "a", "b", "c", "d" )
+for ($i = 0; $i -lt 10; ++$i)
+{
+    echo ( $array | Get-Random )
 }
 
 echo "`n"
